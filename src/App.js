ec6420343f510cde53c24241677dde0fd4f20f32
Cleo Lisboa <cplisboa@gmail.com>
2022-09-08 20:39:44 -0300

    iniciando com rotas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Depto from "./pages/Depto";
+
 function App() {
-  return <>Teste</>;
+  return (
+    <BrowserRouter>
+      {/* AQUI TEREMOS UM MENU */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/depto" element={<Depto />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Depto/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Depto = () => {
+  return <h4>Depto</h4>;
+};
+
+export default Depto;
